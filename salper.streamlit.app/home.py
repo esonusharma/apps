27a33f9344bf9cc123648fbe15e2965ab0b8d9bc
sonu sharma <esonusharma@gmail.com>
2025-04-05 20:22:14 +0530

    update

--- salper.streamlit.app/home.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.title("Slow Advanced Learners")
-st.text('This app facilitates slow advanced learners activities')
-st.text('This code/service/product is subject to the terms of the MIT License')
