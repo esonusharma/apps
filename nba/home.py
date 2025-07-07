48face79dd31e3b0b5b6a116b54219153977b056
esonusharma <esonusharma@gmail.com>
2025-07-07 10:56:31 +0530

    update

---
+++ nba/home.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("NBA Automation")
+st.text('This app hosts multiple apps for automating NBA processes')
+st.text('This code/calculator/service/product is subject to the terms of the MIT License')
