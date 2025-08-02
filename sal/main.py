9f6000ae2071f78dd8f1fc16744c9affbbb10b31
esonusharma <esonusharma@gmail.com>
2025-08-02 10:20:20 +0530

    update

--- sal/main.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.title("SAL Utilities")
-st.text('This app hosts multiple utilities')
-st.text('This code/calculator/service/product is subject to the terms of the MIT License')
